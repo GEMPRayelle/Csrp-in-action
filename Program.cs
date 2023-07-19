fa2a31bd44e0cc33b9355988a81fdf998b343d79
Rayelle <equation0703@naver.com>
2023-07-19 16:40:16 +0900

    깃커밋 테스트

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+/* f12 = 함수 원본 찾아감
+   alt f12 = 중간에서 함수 원본 봄
+   ctrl shift r = 작성한 코드를 함수로 싸매기
+   f2 = 변수이름과 관련된 모든걸 rename 시킴
+   alt + 클릭 = 다중 클릭(클릭한 부분 동시에 타이핑가능)
+*/
+namespace CsharpKitTest{
+    class Program{
+        static void Main(string[] args)
+        {
+            Console.WriteLine("kit test successful2");
+        }
+    }
+}
+
