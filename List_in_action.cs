1c21356e95dbf9eaef576fd17ea438048f3ec4a1
Rayelle <equation0703@naver.com>
2024-01-14 15:34:12 +0900

    리스트 사용법 0.2

--- List_in_action.cs
+++ List_in_action.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections.Generic;//리스트를 사용하기위해 이 네임스페이스를 선언해야함
 
 namespace CsharpKitTest{
-
     class List_in_action
     {
         class Student{
@@ -10,7 +9,16 @@ namespace CsharpKitTest{
             public string name;
         }
 
+        public int[] scores = new int[10];
+
         static void Main(string[] args){
+
+            //배열같은 경우는 크기를 늘리기위해 다시 공간을 할당하면
+            //기존에 있던 배열의 정보는 전부다 날아가고 새로 생성된다 -> 배열의 한계점(실시간 불가능)
+            scores = new int[11];
+
+            //리스트는 배열의 확장으로서 여러 게임 오브젝트를 한 번에 다룰수있다
+
             //리스트 선언 방식
             List<Student> students = new List<Student>();
             //생성자를 사용하여 초기화를 할 수 있다
